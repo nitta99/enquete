7632470424fca574d24582c964b9805df4c66b15
nitta <r.nitta@wisdom.co.jp>
2021-11-01 11:14:59 +0900

    入力チェックとファイル出力の追加

--- index.php
+++ index.php
@@ -3,17 +3,76 @@
 <head>
 <meta charset="utf-8">
 <title>アンケート画面</title>
+
+<script type="text/javascript">
+
+function checkName(){
+	var flag = 0;
+	// 設定開始（必須にする項目を設定してください）
+	if(document.enquete.name.value == ""){ // 「お名前」の入力をチェック
+		flag = 1;
+	}
+	// 設定終了
+	if(flag){
+		window.alert('氏名は必須です'); // 入力漏れがあれば警告ダイアログを表示
+		return false; // 送信を中止
+	}
+	else{
+		return true; // 送信を実行
+	}
+}
+
+function checkNumber(){
+    var flag = 0;
+    // 設定開始（チェックする項目を設定してください）
+    if(document.enquete.age.value.match(/[^0-9]+/)){
+        flag = 1;
+    }
+    // 設定終了
+    if(flag){
+        window.alert('年齢は数字で入力してください'); // 数字以外が入力された場合は警告ダイアログを表示
+        return false; // 送信を中止
+    }else{
+       return true; // 送信を実行
+    }
+}
+</script>
 </head>
 <body>
-<form action="result.php" method="post" >
-<p>氏名<input type="text" name="name" value=""></p>
-<p>年齢<input type="text" name="age" value="">歳</p>
-<p>性別<select name="gender"><option selected="selected">男性</option><option>女性</option></select></p>
-<p>住所<input type="text" name="address" value=""></p>
-<p>電話番号<input type="text" name="telephone" value=""></p>
-<p>メールアドレス<input type="text" name="mail" value=""></p>
-<p>感想<input type="radio" name="thoughts" value="good">良い<input type="radio" name="thoughts" value="normal">普通<input type="radio" name="thoughts" value="bad">悪い</p>
-<p><button type="submit">登録</button></p>
+<form action="result.php" method="post" name='enquete' onSubmit="return (checkName() && checkNumber())">
+<table>
+    <tr>
+        <td>氏名</td>
+        <td><input type="text" name="name" value=""></td>
+    </tr>
+    <tr>
+        <td>年齢</td>
+        <td><input type="text" name="age" value="">歳</td>
+    </tr>
+    <tr>
+        <td>性別</td>
+        <td><select name="gender"><option selected="selected">男性</option><option>女性</option></select></td>
+    </tr>
+    <tr>
+        <td>住所</td>
+        <td><input type="text" name="address" value=""></td>
+    </tr>
+    <tr>
+        <td>電話番号</td>
+        <td><input type="text" name="telephone" value=""></td>
+    </tr>
+    <tr>
+        <td>メールアドレス</td>
+        <td><input type="text" name="mail" value=""></td>
+    </tr>
+    <tr>
+        <td>感想</td>
+        <td><input type="radio" name="thoughts" value="good">良い<input type="radio" name="thoughts" value="normal">普通<input type="radio" name="thoughts" value="bad">悪い</td>
+    </tr>
+	<tr>
+		<td><button type="submit">登録</button></td>
+	</tr>
+</table>
 </form>
 </body>
 </html>
--- result.php
+++ result.php
@@ -5,12 +5,57 @@
 <title>結果表示画面</title>
 </head>
 <body>
-<p>氏名<?php echo htmlspecialchars(@$_POST['name'], ENT_QUOTES, 'UTF-8'); ?></p>
-<p>年齢<?php echo (int)@$_POST['age']; ?> 歳</p>
-<p>性別<?php echo htmlspecialchars(@$_POST['gender'], ENT_QUOTES, 'UTF-8');?></p>
-<p>住所<?php echo htmlspecialchars(@$_POST['address'], ENT_QUOTES, 'UTF-8'); ?></p>
-<p>電話番号<?php echo htmlspecialchars(@$_POST['telephone'], ENT_QUOTES, 'UTF-8'); ?></p>
-<p>メールアドレス<?php echo htmlspecialchars(@$_POST['mail'], ENT_QUOTES, 'UTF-8'); ?></p>
-<p>感想<?php echo htmlspecialchars(@$_POST['thoughts'], ENT_QUOTES, 'UTF-8');?></p>
+<table>
+    <tr>
+        <td>氏名</td>
+        <td><?php echo htmlspecialchars(@$_POST['name'], ENT_QUOTES, 'UTF-8'); ?></td>
+    </tr>
+    <tr>
+        <td>年齢</td>
+        <td><?php echo (int)@$_POST['age']; ?>歳</td>
+    </tr>
+    <tr>
+        <td>性別</td>
+        <td><?php echo htmlspecialchars(@$_POST['gender'], ENT_QUOTES, 'UTF-8');?></td>
+    </tr>
+    <tr>
+        <td>住所</td>
+        <td><?php echo htmlspecialchars(@$_POST['address'], ENT_QUOTES, 'UTF-8'); ?></td>
+    </tr>
+    <tr>
+        <td>電話番号</td>
+        <td><?php echo htmlspecialchars(@$_POST['telephone'], ENT_QUOTES, 'UTF-8'); ?></td>
+    </tr>
+    <tr>
+        <td>メールアドレス</td>
+        <td><?php echo htmlspecialchars(@$_POST['mail'], ENT_QUOTES, 'UTF-8'); ?></td>
+    </tr>
+    <tr>
+        <td>感想</td>
+        <td><?php echo htmlspecialchars(@$_POST['thoughts'], ENT_QUOTES, 'UTF-8');?></td>
+    </tr>
+</table>
+<?php
+
+// 書き込み対象のCSVファイルを開く
+$fp = fopen("data.csv", "w");
+
+$NAME = $_POST['name'];
+$AGE = $_POST['age'];
+$GENDER = $_POST['gender'];
+$ADDRESS = $_POST['address'];
+$TELEPHONE = $_POST['telephone'];
+$MAIL = $_POST['mail'];
+$THOUGHTS = $_POST['thoughts'];
+
+//ストリームフィルタ指定
+stream_filter_prepend($fp,'convert.iconv.utf-8/cp932');
+
+// CSVファイルに書き込む
+fwrite($fp,"$NAME,$AGE,$GENDER,$ADDRESS,$TELEPHONE,$MAIL,$THOUGHTS");
+
+// 書き込み対象のファイルをクローズ
+fclose($fp);
+?>
 </body>
 </html>
