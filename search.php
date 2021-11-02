e9c23aaec2222b7b8ecb3c3f16ddee39de934fa0
nitta <r.nitta@wisdom.co.jp>
2021-11-02 10:50:41 +0900

    修正

--- search.php
+++ search.php
@@ -60,7 +60,7 @@ echo '<table border="1">
 
 // while文でCSVファイルのデータを1つずつ繰り返し読み込む
 while($data = fgetcsv($fp)){
-
+    mb_convert_variables("UTF-8", "SJIS", $data);
     // テーブルセルに配列の値を格納
     echo '<tr>';
     echo '<td>'.$data[0].'</td>';
