309634881a6eed0da1db887576fcd57e414e0b0b
nitta <r.nitta@wisdom.co.jp>
2021-11-04 11:44:25 +0900

    composer.json の追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '54670b8f28da6ea719d5e25821c6cbce3094557e',
+        'name' => 'enquete/enquete',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'enquete/enquete' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '54670b8f28da6ea719d5e25821c6cbce3094557e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
